91aa94c72e436cbe5cbc1862ccefb858cd35ed76
Dominik <dominik.malantowicz@gmail.com>
2017-02-20 22:55:37 +0100

    Fix emulator version

--- scripts/emulator.sh
+++ scripts/emulator.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo no | android create avd --force -n test -t android-19 --abi armeabi-v7a --skin 480x800
+echo no | android create avd --force -n test -t android-21 --abi armeabi-v7a --skin 480x800
 emulator -avd test -no-audio -no-window &
 android-wait-for-emulator
 adb shell settings put global window_animation_scale 0 &
