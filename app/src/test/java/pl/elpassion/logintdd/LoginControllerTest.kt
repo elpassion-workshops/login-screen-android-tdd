bc53085876c24935b5389d8e2c4b2b12d8ada27f
Dominik <dominik.malantowicz@gmail.com>
2017-02-21 20:19:28 +0100

    [troll commit] Restrict production code implementation

--- app/src/test/java/pl/elpassion/logintdd/LoginControllerTest.kt
+++ app/src/test/java/pl/elpassion/logintdd/LoginControllerTest.kt
@@ -143,7 +143,7 @@ class LoginController(val api: Login.Api, val view: Login.View) {
         when {
             login.isBlank() -> view.showEmptyLoginError()
             password.isBlank() -> view.showEmptyPasswordError()
-            else -> login(login, password)
+            login.isNotBlank() -> login(login, password)
         }
     }
 
