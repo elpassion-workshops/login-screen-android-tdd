cbb1e54f2b6f711eb12a8d7398b1084c5db84c5e
Kasper Kondzielski <kasper.kondzielski@elpassion.pl>
2017-02-26 15:58:54 +0100

    Show login error only when api fails

--- app/src/main/java/pl/elpassion/logintdd/login/LoginActivity.kt
+++ app/src/main/java/pl/elpassion/logintdd/login/LoginActivity.kt
@@ -3,7 +3,6 @@ package pl.elpassion.logintdd.login
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.elpassion.android.view.show
-import io.reactivex.Single
 import kotlinx.android.synthetic.main.login_activity.*
 import pl.elpassion.logintdd.R
 
@@ -12,9 +11,7 @@ class LoginActivity : AppCompatActivity(), Login.View {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
         loginButton.setOnClickListener {
-            LoginController(object : Login.Api {
-                override fun login(login: String, password: String) = Single.error<User>(RuntimeException())
-            }, this, object : Login.UserRepository {
+            LoginController(Login.Api.get(), this, object : Login.UserRepository {
                 override fun saveUser(user: User) = Unit
             }).onLogin(loginInput.text.toString(), passwordInput.text.toString())
         }
@@ -28,9 +25,7 @@ class LoginActivity : AppCompatActivity(), Login.View {
         passwordEmptyError.show()
     }
 
-    override fun openNextScreen() {
-        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun openNextScreen() = Unit
 
     override fun showLoginFailed() {
         apiError.show()
