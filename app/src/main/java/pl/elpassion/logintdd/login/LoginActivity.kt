23c19d1e1e225022f97d2aa68dc29a6327b0a7b3
Kasper Kondzielski <kasper.kondzielski@elpassion.pl>
2017-02-26 13:56:09 +0100

    Show login empty error only after click

--- app/src/main/java/pl/elpassion/logintdd/login/LoginActivity.kt
+++ app/src/main/java/pl/elpassion/logintdd/login/LoginActivity.kt
@@ -2,12 +2,45 @@ package pl.elpassion.logintdd.login
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.elpassion.android.view.show
+import io.reactivex.Single
+import kotlinx.android.synthetic.main.login_activity.*
 import pl.elpassion.logintdd.R
 
-class LoginActivity : AppCompatActivity() {
-
+class LoginActivity : AppCompatActivity(), Login.View {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
+        loginButton.setOnClickListener {
+            LoginController(object : Login.Api {
+                override fun login(login: String, password: String) = Single.never<User>()
+            }, this, object : Login.UserRepository {
+                override fun saveUser(user: User) = Unit
+            }).onLogin("","")
+        }
+    }
+
+    override fun showEmptyLoginError() {
+        loginEmptyError.show()
+    }
+
+    override fun showEmptyPasswordError() {
+        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun openNextScreen() {
+        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showLoginFailed() {
+        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showLoader() {
+        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun hideLoader() {
+        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
