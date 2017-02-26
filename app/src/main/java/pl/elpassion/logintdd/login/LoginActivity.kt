336f53c9e2e62fd4a7f78e0aa99a6e8871b1cb44
Kasper Kondzielski <kasper.kondzielski@elpassion.pl>
2017-02-26 13:10:30 +0100

    Show login header

--- app/src/main/java/pl/elpassion/logintdd/login/LoginActivity.kt
+++ app/src/main/java/pl/elpassion/logintdd/login/LoginActivity.kt
@@ -1,5 +1,13 @@
 package pl.elpassion.logintdd.login
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import pl.elpassion.logintdd.R
 
-class LoginActivity : AppCompatActivity()
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity)
+    }
+}
