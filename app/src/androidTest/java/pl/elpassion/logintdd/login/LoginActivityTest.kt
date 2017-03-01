09273e842045f5d59838f6f05ebacef44409050f
Karol Kowalski <ordon1313@gmail.com>
2017-03-01 14:09:22 +0100

    Have login input header

--- app/src/androidTest/java/pl/elpassion/logintdd/login/LoginActivityTest.kt
+++ app/src/androidTest/java/pl/elpassion/logintdd/login/LoginActivityTest.kt
@@ -1,6 +1,8 @@
 package pl.elpassion.logintdd.login
 
 import android.support.test.rule.ActivityTestRule
+import com.elpassion.android.commons.espresso.isDisplayed
+import com.elpassion.android.commons.espresso.onText
 import org.junit.Rule
 import org.junit.Test
 
@@ -12,5 +14,10 @@ class LoginActivityTest {
     @Test
     fun shouldStartLoginActivity() {
 
+    }
+
+    @Test
+    fun shouldHaveLoginInputHeader() {
+        onText("Login").isDisplayed()
     }
 }
