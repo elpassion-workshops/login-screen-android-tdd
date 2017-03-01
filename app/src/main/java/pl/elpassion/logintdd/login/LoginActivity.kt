08b1bd325d1b0a07008a88fc59f554d6f9ac30b9
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2017-03-01 15:50:35 +0100

    Harness of login controller

--- app/src/main/java/pl/elpassion/logintdd/login/LoginActivity.kt
+++ app/src/main/java/pl/elpassion/logintdd/login/LoginActivity.kt
@@ -3,14 +3,48 @@ package pl.elpassion.logintdd.login
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.login_activity.*
 import pl.elpassion.logintdd.R
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), Login.View {
+	override fun showEmptyLoginError() {
+		emptyLoginErrorTextView.visibility = View.VISIBLE
+	}
+
+	override fun showEmptyPasswordError() {
+		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+	}
+
+	override fun openNextScreen() {
+		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+	}
+
+	override fun showLoginFailed() {
+		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+	}
+
+	override fun showLoader() {
+		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+	}
+
+	override fun hideLoader() {
+		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+	}
+
+	val loginController = LoginController(object : Login.Api {
+		override fun login(login: String, password: String) = Single.never<User>()
+
+	}, this, object : Login.UserRepository {
+		override fun saveUser(user: User) = Unit
+	}, Schedulers.io(), AndroidSchedulers.mainThread())
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.login_activity)
-		loginButton.setOnClickListener { emptyLoginErrorTextView.visibility = View.VISIBLE }
+
+		loginButton.setOnClickListener { loginController.onLogin(loginInput.text.toString(), passwordInput.text.toString()) }
 	}
 }
