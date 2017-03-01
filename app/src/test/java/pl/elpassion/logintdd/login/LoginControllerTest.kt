1882bceb387d3d664d4645207494bb9f65c18b1b
Bruno Wieczorek <bruno.wieczorek@droidsonroids.pl>
2017-03-01 10:53:35 +0100

    Not show password empty error

--- app/src/test/java/pl/elpassion/logintdd/login/LoginControllerTest.kt
+++ app/src/test/java/pl/elpassion/logintdd/login/LoginControllerTest.kt
@@ -27,8 +27,14 @@ class LoginControllerTest {
         verify(view).showPasswordEmptyError()
     }
 
+    @Test
+    fun shouldNotShowEmptyPasswordError() {
+        login(password = "password")
+        verify(view, never()).showPasswordEmptyError()
+    }
+
     private fun login(login: String = "myLogin", password: String = "password") {
-        LoginController(view).login(login = login)
+        LoginController(view).login(login = login, password = password)
     }
 }
 
@@ -40,10 +46,12 @@ interface Login {
 }
 
 class LoginController(private val view: Login.View) {
-    fun login(login: String) {
+    fun login(login: String, password: String) {
         if (login.isEmpty()) {
             view.showLoginEmptyError()
         }
-        view.showPasswordEmptyError()
+        if (password.isEmpty()) {
+            view.showPasswordEmptyError()
+        }
     }
 }
