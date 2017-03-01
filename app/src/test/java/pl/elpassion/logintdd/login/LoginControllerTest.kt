bd8dc64d12006d08ed2e7e67ecfd8fa174632375
Karol Kowalski <ordon1313@gmail.com>
2017-03-01 10:06:52 +0100

    Show error when login is empty

--- app/src/test/java/pl/elpassion/logintdd/login/LoginControllerTest.kt
+++ app/src/test/java/pl/elpassion/logintdd/login/LoginControllerTest.kt
@@ -1,13 +1,33 @@
 package pl.elpassion.logintdd.login
 
+import com.nhaarman.mockito_kotlin.mock
 import org.junit.Test
+import org.mockito.Mockito.verify
 
 class LoginControllerTest {
 
     @Test
     fun shouldHaveLoginController() {
-        LoginController()
+        val view = mock<Login.View>()
+        LoginController(view)
+    }
+
+    @Test
+    fun shouldShowErrorWhenLoginIsEmpty() {
+        val view = mock<Login.View>()
+        LoginController(view).login(login = "")
+        verify(view).showLoginEmptyError()
     }
 }
 
-class LoginController
+interface Login {
+    interface View {
+        fun showLoginEmptyError()
+    }
+}
+
+class LoginController(private val view: Login.View) {
+    fun login(login: String) {
+        view.showLoginEmptyError()
+    }
+}
