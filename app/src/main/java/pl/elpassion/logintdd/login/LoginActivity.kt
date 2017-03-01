c26ee8956ab4a6232fdd05666702011d1f586536
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2017-03-01 16:16:15 +0100

    [Refactor] Login controller interaction extracted to methods

--- app/src/main/java/pl/elpassion/logintdd/login/LoginActivity.kt
+++ app/src/main/java/pl/elpassion/logintdd/login/LoginActivity.kt
@@ -1,8 +1,10 @@
 package pl.elpassion.logintdd.login
 
 import android.os.Bundle
+
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.TextView
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
@@ -10,6 +12,14 @@ import kotlinx.android.synthetic.main.login_activity.*
 import pl.elpassion.logintdd.R
 
 class LoginActivity : AppCompatActivity(), Login.View {
+
+	val loginController = LoginController(object : Login.Api {
+		override fun login(login: String, password: String) = Single.never<User>()
+
+	}, this, object : Login.UserRepository {
+		override fun saveUser(user: User) = Unit
+	}, Schedulers.io(), AndroidSchedulers.mainThread())
+
 	override fun showEmptyLoginError() {
 		emptyLoginErrorTextView.visibility = View.VISIBLE
 	}
@@ -34,17 +44,14 @@ class LoginActivity : AppCompatActivity(), Login.View {
 		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
 	}
 
-	val loginController = LoginController(object : Login.Api {
-		override fun login(login: String, password: String) = Single.never<User>()
-
-	}, this, object : Login.UserRepository {
-		override fun saveUser(user: User) = Unit
-	}, Schedulers.io(), AndroidSchedulers.mainThread())
-
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.login_activity)
 
-		loginButton.setOnClickListener { loginController.onLogin(loginInput.text.toString(), passwordInput.text.toString()) }
+		loginButton.setOnClickListener { onSignInClicked() }
 	}
+
+	private fun TextView.stringText() = text.toString()
+
+	private fun onSignInClicked() = loginController.onLogin(loginInput.stringText(), passwordInput.stringText())
 }
