fcabb884740e8eaa8629e956f7bd1fbf79917fe5
Sebastian Staszyński <sebastian.staszynski@droidsonroids.pl>
2017-03-01 14:36:42 +0100

    Password is displayed in secure way

--- app/src/androidTest/java/pl/elpassion/logintdd/login/LoginActivityTest.kt
+++ app/src/androidTest/java/pl/elpassion/logintdd/login/LoginActivityTest.kt
@@ -1,6 +1,10 @@
 package pl.elpassion.logintdd.login
 
+import android.support.test.espresso.ViewInteraction
+import android.support.test.espresso.assertion.ViewAssertions
+import android.support.test.espresso.matcher.ViewMatchers
 import android.support.test.rule.ActivityTestRule
+import android.text.InputType
 import com.elpassion.android.commons.espresso.*
 import org.junit.Rule
 import org.junit.Test
@@ -24,5 +28,19 @@ class LoginActivityTest {
     @Test
     fun shouldHavePasswordLabel() {
         onText("Password").isDisplayed()
+    }
+
+    @Test
+    fun shouldHavePasswordInput() {
+        onId(R.id.passwordInput).typeText("password").hasText("password")
+    }
+
+    @Test
+    fun shouldPasswordBeSecure() {
+        onId(R.id.passwordInput).isSecure()
+    }
+
+    fun ViewInteraction.isSecure() {
+        check(ViewAssertions.matches(ViewMatchers.withInputType(InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD)))
     }
 }
