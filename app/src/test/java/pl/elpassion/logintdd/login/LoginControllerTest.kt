7d82830a8a1c827737302d046961d6e883791c67
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2017-03-01 11:00:03 +0100

    Call Api when credentials are valid

--- app/src/test/java/pl/elpassion/logintdd/login/LoginControllerTest.kt
+++ app/src/test/java/pl/elpassion/logintdd/login/LoginControllerTest.kt
@@ -3,53 +3,67 @@ package pl.elpassion.logintdd.login
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.never
 import org.junit.Test
+import org.mockito.Matchers
 import org.mockito.Mockito.verify
 
 class LoginControllerTest {
 
-    private val view = mock<Login.View>()
+	private val view = mock<Login.View>()
+	private val api = mock<Login.Api>()
 
-    @Test
-    fun shouldShowErrorWhenLoginIsEmpty() {
-        login(login = "")
-        verify(view).showLoginEmptyError()
-    }
+	@Test
+	fun shouldShowErrorWhenLoginIsEmpty() {
+		login(login = "")
+		verify(view).showLoginEmptyError()
+	}
 
-    @Test
-    fun shouldNotShowErrorWhenLoginIsNotEmpty() {
-        login(login = "myLogin")
-        verify(view, never()).showLoginEmptyError()
-    }
+	@Test
+	fun shouldNotShowErrorWhenLoginIsNotEmpty() {
+		login(login = "myLogin")
+		verify(view, never()).showLoginEmptyError()
+	}
 
-    @Test
-    fun shouldShowErrorWhenPasswordIsEmpty() {
-        login(password = "")
-        verify(view).showPasswordEmptyError()
-    }
+	@Test
+	fun shouldShowErrorWhenPasswordIsEmpty() {
+		login(password = "")
+		verify(view).showPasswordEmptyError()
+	}
 
-    @Test
-    fun shouldNotShowErrorWhenPasswordIsNotEmpty() {
-        login(password = "password")
-        verify(view, never()).showPasswordEmptyError()
-    }
+	@Test
+	fun shouldNotShowErrorWhenPasswordIsNotEmpty() {
+		login(password = "password")
+		verify(view, never()).showPasswordEmptyError()
+	}
 
-    private fun login(login: String = "login", password: String = "password") {
-        LoginController(view).login(login = login, password = password)
-    }
+	@Test
+	fun shouldCallApiWhenCredentialsAreValid() {
+		login()
+		verify(api).login()
+	}
+
+	private fun login(login: String = "login", password: String = "password") {
+		LoginController(view, api).login(login = login, password = password)
+	}
 }
 
 interface Login {
-    interface View {
-        fun showLoginEmptyError()
-        fun showPasswordEmptyError()
-    }
+	interface View {
+		fun showLoginEmptyError()
+		fun showPasswordEmptyError()
+	}
+
+	interface Api {
+		fun login()
+	}
 }
 
-class LoginController(private val view: Login.View) {
-    fun login(login: String, password: String) {
-        when {
-            login.isEmpty() -> view.showLoginEmptyError()
-            password.isEmpty() -> view.showPasswordEmptyError()
-        }
-    }
+class LoginController(private val view: Login.View, private val api: Login.Api) {
+
+	fun login(login: String, password: String) {
+		when {
+			login.isEmpty() -> view.showLoginEmptyError()
+			password.isEmpty() -> view.showPasswordEmptyError()
+			else -> api.login()
+		}
+	}
 }
