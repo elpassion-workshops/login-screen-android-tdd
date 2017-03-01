be8b9598755efbea1464fb8860d90a50ad98a73b
Karol Kowalski <ordon1313@gmail.com>
2017-03-01 14:03:17 +0100

    Start login activity

---
+++ app/src/androidTest/java/pl/elpassion/logintdd/login/LoginActivityTest.kt
@@ -0,0 +1,16 @@
+package pl.elpassion.logintdd.login
+
+import android.support.test.rule.ActivityTestRule
+import org.junit.Rule
+import org.junit.Test
+
+class LoginActivityTest {
+
+    @JvmField @Rule
+    val rule = ActivityTestRule<LoginActivity>(LoginActivity::class.java)
+
+    @Test
+    fun shouldStartLoginActivity() {
+
+    }
+}
