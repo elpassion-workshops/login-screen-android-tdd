d00b27b83f7a708a0173384f81b9bd04b5fa3c39
Adam Łastowski <adam.lastowski@droidsonroids.pl>
2017-03-01 15:03:15 +0100

    Have password be secure

--- app/src/androidTest/java/pl/elpassion/logintdd/login/LoginActivityTest.kt
+++ app/src/androidTest/java/pl/elpassion/logintdd/login/LoginActivityTest.kt
@@ -1,6 +1,10 @@
 package pl.elpassion.logintdd.login
 
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers
+import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.rule.ActivityTestRule
+import android.text.InputType
 import com.elpassion.android.commons.espresso.*
 import org.junit.Rule
 import org.junit.Test
@@ -29,5 +33,10 @@ class LoginActivityTest {
     @Test
     fun shouldHavePasswordHeader() {
         onText("Password").isDisplayed()
+    }
+
+    @Test
+    fun shouldPasswordBeSecure() {
+        onId(R.id.passwordInput).check(matches(withInputType(InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD)))
     }
 }
